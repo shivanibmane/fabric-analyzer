27f37aa990936d911b991101d344edfb000dc29b
ShivaniMane <shivanimane43@gmail.com>
2025-06-12 10:41:07 +0530

    Initial commit with project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1>Fabric Analyzer</h1>
+    </div>
+  )
+}
+
+export default App
